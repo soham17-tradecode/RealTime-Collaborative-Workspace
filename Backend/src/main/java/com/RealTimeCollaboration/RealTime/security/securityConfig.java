9ada743c02053be87ef38084905e647fd43ec078
soham <sohambasak17@gmail.com>
2026-09-08 01:33:34 +0530

    op

--- Backend/src/main/java/com/RealTimeCollaboration/RealTime/security/securityConfig.java
+++ Backend/src/main/java/com/RealTimeCollaboration/RealTime/security/securityConfig.java
@@ -33,7 +33,7 @@ public class securityConfig {
     private userDetailsService userDetailsService;
 
     @Autowired
-    private jwt Filter jwtFilter;
+    private jwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
